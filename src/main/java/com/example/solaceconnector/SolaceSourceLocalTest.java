6eed0d528e93898c83d6ce4e9def1ce8d1798333
Marmik123 <marmikmodi209@gmail.com>
2025-01-17 20:37:47 +0530

    +Updated the DDL and downgraded the java to 11

--- src/main/java/com/example/solaceconnector/SolaceSourceLocalTest.java
+++ src/main/java/com/example/solaceconnector/SolaceSourceLocalTest.java
@@ -15,11 +15,83 @@ public class SolaceSourceLocalTest {
         // Step 2: Initialize the Flink Table Environment
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
-        // Step 3: Register the Custom Source Connector - Mention solace connection details here 
-        tableEnv.executeSql(
-            "CREATE TABLE T (" +
-            "  subject STRING" +
-            
+        String ddl = "CREATE TABLE T (" +
+            "  before ROW(" +
+            "    DAT_TXN TIMESTAMP(3)," +
+            "    COD_CC_BRN_TXN INT," +
+            "    COD_USERNO INT," +
+            "    CTR_BATCH_NO INT," +
+            "    REF_SYS_TR_AUD_NO BIGINT," +
+            "    REF_SUB_SEQ_NO INT," +
+            "    COD_ACCT_NO STRING," +
+            "    TXT_TXN_DESC STRING," +
+            "    DAT_VALUE TIMESTAMP(3)," +
+            "    REF_CHQ_NO STRING," +
+            "    COD_DRCR STRING," +
+            "    COD_TXN_MNEMONIC INT," +
+            "    COD_TXN_LITERAL STRING," +
+            "    AMT_TXN DOUBLE," +
+            "    FLG_PASBKUPD STRING," +
+            "    RAT_CCY INT," +
+            "    COD_MSG_TYP INT," +
+            "    COD_PROC INT," +
+            "    CTR_UPDAT_SRLNO INT," +
+            "    DAT_POST TIMESTAMP(3)," +
+            "    AMT_TXN_TCY DOUBLE," +
+            "    COD_TXN_CCY INT," +
+            "    RAT_CONV_TCLCY INT," +
+            "    COD_SC INT," +
+            "    COD_AUTH_ID STRING," +
+            "    REF_CARD_NO STRING," +
+            "    REF_TXN_NO STRING," +
+            "    REF_USR_NO STRING," +
+            "    TXT_ADDL_INFO STRING," +
+            "    DAT_TS_TXN_INIT TIMESTAMP(3)," +
+            "    DAT_TS_SENT_FOR_AUTH TIMESTAMP(3)," +
+            "    DAT_TS_AUTH TIMESTAMP(3)," +
+            "    DAT_TS_SUB_FOR_PROCESSING TIMESTAMP(3)," +
+            "    DAT_TS_SENT_TO_HOST TIMESTAMP(3)," +
+            "    DAT_TS_HOST_PROCESSING TIMESTAMP(3)," +
+            "    COD_ENTITY_VPD INT" +
+            "  )," +
+            "  after ROW(" +
+            "    DAT_TXN TIMESTAMP(3)," +
+            "    COD_CC_BRN_TXN INT," +
+            "    COD_USERNO INT," +
+            "    CTR_BATCH_NO INT," +
+            "    REF_SYS_TR_AUD_NO BIGINT," +
+            "    REF_SUB_SEQ_NO INT," +
+            "    COD_ACCT_NO STRING," +
+            "    TXT_TXN_DESC STRING," +
+            "    DAT_VALUE TIMESTAMP(3)," +
+            "    REF_CHQ_NO STRING," +
+            "    COD_DRCR STRING," +
+            "    COD_TXN_MNEMONIC INT," +
+            "    COD_TXN_LITERAL STRING," +
+            "    AMT_TXN DOUBLE," +
+            "    FLG_PASBKUPD STRING," +
+            "    RAT_CCY INT," +
+            "    COD_MSG_TYP INT," +
+            "    COD_PROC INT," +
+            "    CTR_UPDAT_SRLNO INT," +
+            "    DAT_POST TIMESTAMP(3)," +
+            "    AMT_TXN_TCY DOUBLE," +
+            "    COD_TXN_CCY INT," +
+            "    RAT_CONV_TCLCY INT," +
+            "    COD_SC INT," +
+            "    COD_AUTH_ID STRING," +
+            "    REF_CARD_NO STRING," +
+            "    REF_TXN_NO STRING," +
+            "    REF_USR_NO STRING," +
+            "    TXT_ADDL_INFO STRING," +
+            "    DAT_TS_TXN_INIT TIMESTAMP(3)," +
+            "    DAT_TS_SENT_FOR_AUTH TIMESTAMP(3)," +
+            "    DAT_TS_AUTH TIMESTAMP(3)," +
+            "    DAT_TS_SUB_FOR_PROCESSING TIMESTAMP(3)," +
+            "    DAT_TS_SENT_TO_HOST TIMESTAMP(3)," +
+            "    DAT_TS_HOST_PROCESSING TIMESTAMP(3)," +
+            "    COD_ENTITY_VPD INT" +
+            "  )" +
             ") WITH (" +
             "  'connector' = 'solace'," +
             "  'host' = 'https://10.226.183.137:55555'," +
@@ -27,8 +99,25 @@ public class SolaceSourceLocalTest {
             "  'username' = 'datalakeuser'," +
             "  'password' = 'hdfcbank123$'," +
             "  'queue' = 'q.hdfc.cdc.flexcube.flink.gm'" +
-            ")"
-        );
+            ")";
+
+
+        // Step 3: Register the Custom Source Connector - Mention solace connection details here 
+        // tableEnv.executeSql(
+        //     "CREATE TABLE T (" +
+        //     "  subject STRING" +
+            
+        //     ") WITH (" +
+        //     "  'connector' = 'solace'," +
+        //     "  'host' = 'https://10.226.183.137:55555'," +
+        //     "  'vpn' = 'cdc'," +
+        //     "  'username' = 'datalakeuser'," +
+        //     "  'password' = 'hdfcbank123$'," +
+        //     "  'queue' = 'q.hdfc.cdc.flexcube.flink.gm'" +
+        //     ")"
+        // );
+
+        tableEnv.executeSql(ddl);
 
         // Debug: Confirm the table creation
         tableEnv.executeSql("SHOW TABLES").print();
