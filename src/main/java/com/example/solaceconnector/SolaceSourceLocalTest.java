9289637a0461a757a2f25b28757ba15307585dff
Marmik123 <marmikmodi209@gmail.com>
2025-01-19 16:07:21 +0530

    changed the java to 1.8

--- src/main/java/com/example/solaceconnector/SolaceSourceLocalTest.java
+++ src/main/java/com/example/solaceconnector/SolaceSourceLocalTest.java
@@ -10,105 +10,105 @@ public class SolaceSourceLocalTest {
     public static void main(String[] args) throws Exception {
         // Step 1: Initialize the Flink Execution Environment
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(2); // Adjust parallelism for local testing
+        env.setParallelism(1); // Adjust parallelism for local testing
 
         // Step 2: Initialize the Flink Table Environment
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
-        String ddl = "CREATE TABLE T (" +
-            "  before ROW(" +
-            "    DAT_TXN TIMESTAMP(3)," +
-            "    COD_CC_BRN_TXN INT," +
-            "    COD_USERNO INT," +
-            "    CTR_BATCH_NO INT," +
-            "    REF_SYS_TR_AUD_NO BIGINT," +
-            "    REF_SUB_SEQ_NO INT," +
-            "    COD_ACCT_NO STRING," +
-            "    TXT_TXN_DESC STRING," +
-            "    DAT_VALUE TIMESTAMP(3)," +
-            "    REF_CHQ_NO STRING," +
-            "    COD_DRCR STRING," +
-            "    COD_TXN_MNEMONIC INT," +
-            "    COD_TXN_LITERAL STRING," +
-            "    AMT_TXN DOUBLE," +
-            "    FLG_PASBKUPD STRING," +
-            "    RAT_CCY INT," +
-            "    COD_MSG_TYP INT," +
-            "    COD_PROC INT," +
-            "    CTR_UPDAT_SRLNO INT," +
-            "    DAT_POST TIMESTAMP(3)," +
-            "    AMT_TXN_TCY DOUBLE," +
-            "    COD_TXN_CCY INT," +
-            "    RAT_CONV_TCLCY INT," +
-            "    COD_SC INT," +
-            "    COD_AUTH_ID STRING," +
-            "    REF_CARD_NO STRING," +
-            "    REF_TXN_NO STRING," +
-            "    REF_USR_NO STRING," +
-            "    TXT_ADDL_INFO STRING," +
-            "    DAT_TS_TXN_INIT TIMESTAMP(3)," +
-            "    DAT_TS_SENT_FOR_AUTH TIMESTAMP(3)," +
-            "    DAT_TS_AUTH TIMESTAMP(3)," +
-            "    DAT_TS_SUB_FOR_PROCESSING TIMESTAMP(3)," +
-            "    DAT_TS_SENT_TO_HOST TIMESTAMP(3)," +
-            "    DAT_TS_HOST_PROCESSING TIMESTAMP(3)," +
-            "    COD_ENTITY_VPD INT" +
-            "  )," +
-            "  after ROW(" +
-            "    DAT_TXN TIMESTAMP(3)," +
-            "    COD_CC_BRN_TXN INT," +
-            "    COD_USERNO INT," +
-            "    CTR_BATCH_NO INT," +
-            "    REF_SYS_TR_AUD_NO BIGINT," +
-            "    REF_SUB_SEQ_NO INT," +
-            "    COD_ACCT_NO STRING," +
-            "    TXT_TXN_DESC STRING," +
-            "    DAT_VALUE TIMESTAMP(3)," +
-            "    REF_CHQ_NO STRING," +
-            "    COD_DRCR STRING," +
-            "    COD_TXN_MNEMONIC INT," +
-            "    COD_TXN_LITERAL STRING," +
-            "    AMT_TXN DOUBLE," +
-            "    FLG_PASBKUPD STRING," +
-            "    RAT_CCY INT," +
-            "    COD_MSG_TYP INT," +
-            "    COD_PROC INT," +
-            "    CTR_UPDAT_SRLNO INT," +
-            "    DAT_POST TIMESTAMP(3)," +
-            "    AMT_TXN_TCY DOUBLE," +
-            "    COD_TXN_CCY INT," +
-            "    RAT_CONV_TCLCY INT," +
-            "    COD_SC INT," +
-            "    COD_AUTH_ID STRING," +
-            "    REF_CARD_NO STRING," +
-            "    REF_TXN_NO STRING," +
-            "    REF_USR_NO STRING," +
-            "    TXT_ADDL_INFO STRING," +
-            "    DAT_TS_TXN_INIT TIMESTAMP(3)," +
-            "    DAT_TS_SENT_FOR_AUTH TIMESTAMP(3)," +
-            "    DAT_TS_AUTH TIMESTAMP(3)," +
-            "    DAT_TS_SUB_FOR_PROCESSING TIMESTAMP(3)," +
-            "    DAT_TS_SENT_TO_HOST TIMESTAMP(3)," +
-            "    DAT_TS_HOST_PROCESSING TIMESTAMP(3)," +
-            "    COD_ENTITY_VPD INT" +
-            "  )" +
-            ") WITH (" +
-            "  'connector' = 'solace'," +
-            "  'host' = 'https://10.226.183.137:55555'," +
-            "  'vpn' = 'cdc'," +
-            "  'username' = 'datalakeuser'," +
-            "  'password' = 'hdfcbank123$'," +
-            "  'queue' = 'q.hdfc.cdc.flexcube.flink.gm'" +
-            ")";
+        // String ddl = "CREATE TABLE T (" +
+        //     "  before ROW(" +
+        //     "    DAT_TXN TIMESTAMP(3)," +
+        //     "    COD_CC_BRN_TXN INT," +
+        //     "    COD_USERNO INT," +
+        //     "    CTR_BATCH_NO INT," +
+        //     "    REF_SYS_TR_AUD_NO BIGINT," +
+        //     "    REF_SUB_SEQ_NO INT," +  
+        //     "    COD_ACCT_NO STRING," +
+        //     "    TXT_TXN_DESC STRING," +
+        //     "    DAT_VALUE TIMESTAMP(3)," +
+        //     "    REF_CHQ_NO STRING," +
+        //     "    COD_DRCR STRING," +
+        //     "    COD_TXN_MNEMONIC INT," +
+        //     "    COD_TXN_LITERAL STRING," +
+        //     "    AMT_TXN DOUBLE," +
+        //     "    FLG_PASBKUPD STRING," +
+        //     "    RAT_CCY INT," +
+        //     "    COD_MSG_TYP INT," +
+        //     "    COD_PROC INT," +
+        //     "    CTR_UPDAT_SRLNO INT," +
+        //     "    DAT_POST TIMESTAMP(3)," +
+        //     "    AMT_TXN_TCY DOUBLE," +
+        //     "    COD_TXN_CCY INT," +
+        //     "    RAT_CONV_TCLCY INT," +
+        //     "    COD_SC INT," +
+        //     "    COD_AUTH_ID STRING," +
+        //     "    REF_CARD_NO STRING," +
+        //     "    REF_TXN_NO STRING," +
+        //     "    REF_USR_NO STRING," +
+        //     "    TXT_ADDL_INFO STRING," +
+        //     "    DAT_TS_TXN_INIT TIMESTAMP(3)," +
+        //     "    DAT_TS_SENT_FOR_AUTH TIMESTAMP(3)," +
+        //     "    DAT_TS_AUTH TIMESTAMP(3)," +
+        //     "    DAT_TS_SUB_FOR_PROCESSING TIMESTAMP(3)," +
+        //     "    DAT_TS_SENT_TO_HOST TIMESTAMP(3)," +
+        //     "    DAT_TS_HOST_PROCESSING TIMESTAMP(3)," +
+        //     "    COD_ENTITY_VPD INT" +
+        //     "  )," +
+        //     "  after ROW(" +
+        //     "    DAT_TXN TIMESTAMP(3)," +
+        //     "    COD_CC_BRN_TXN INT," +
+        //     "    COD_USERNO INT," +
+        //     "    CTR_BATCH_NO INT," +
+        //     "    REF_SYS_TR_AUD_NO BIGINT," +
+        //     "    REF_SUB_SEQ_NO INT," +
+        //     "    COD_ACCT_NO STRING," +
+        //     "    TXT_TXN_DESC STRING," +
+        //     "    DAT_VALUE TIMESTAMP(3)," +
+        //     "    REF_CHQ_NO STRING," +
+        //     "    COD_DRCR STRING," +
+        //     "    COD_TXN_MNEMONIC INT," +
+        //     "    COD_TXN_LITERAL STRING," +
+        //     "    AMT_TXN DOUBLE," +
+        //     "    FLG_PASBKUPD STRING," +
+        //     "    RAT_CCY INT," +
+        //     "    COD_MSG_TYP INT," +
+        //     "    COD_PROC INT," +
+        //     "    CTR_UPDAT_SRLNO INT," +
+        //     "    DAT_POST TIMESTAMP(3)," +
+        //     "    AMT_TXN_TCY DOUBLE," +
+        //     "    COD_TXN_CCY INT," +
+        //     "    RAT_CONV_TCLCY INT," +
+        //     "    COD_SC INT," +
+        //     "    COD_AUTH_ID STRING," +
+        //     "    REF_CARD_NO STRING," +
+        //     "    REF_TXN_NO STRING," +
+        //     "    REF_USR_NO STRING," +
+        //     "    TXT_ADDL_INFO STRING," +
+        //     "    DAT_TS_TXN_INIT TIMESTAMP(3)," +
+        //     "    DAT_TS_SENT_FOR_AUTH TIMESTAMP(3)," +
+        //     "    DAT_TS_AUTH TIMESTAMP(3)," +
+        //     "    DAT_TS_SUB_FOR_PROCESSING TIMESTAMP(3)," +
+        //     "    DAT_TS_SENT_TO_HOST TIMESTAMP(3)," +
+        //     "    DAT_TS_HOST_PROCESSING TIMESTAMP(3)," +
+        //     "    COD_ENTITY_VPD INT" +
+        //     "  )" +
+        //     ") WITH (" +
+        //     "  'connector' = 'solace'," +
+        //     "  'host' = 'https://10.226.183.137:55555'," +
+        //     "  'vpn' = 'cdc'," +
+        //     "  'username' = 'datalakeuser'," +
+        //     "  'password' = 'hdfcbank123$'," +
+        //     "  'queue' = 'q.hdfc.cdc.flexcube.flink.gm'" +
+        //     ")";
 
         String mainDDL ="CREATE TABLE T ("+
-        "Id STRING"+
-        "Payload STRING"+
-        "PartitionKey STRING"+
-        "Topic STRING"+
-        "TimeStamp TIMESTAMP(3)" +
+        "Id STRING,"+
+        "Payload STRING,"+
+        "PartitionKey STRING,"+
+        "Topic STRING,"+
+        "TimeStamp STRING," +
         "Headers STRING" +
-        "creation_ts TIMESTAMP(3)"+
+        // "creation_ts TIMESTAMP(3)"+
         ") WITH (" +
         "  'connector' = 'solace'," +
         "  'host' = 'https://10.226.183.137:55555'," +
@@ -134,7 +134,7 @@ public class SolaceSourceLocalTest {
         //     ")"
         // );
 
-        tableEnv.executeSql(ddl);
+        tableEnv.executeSql(mainDDL);
 
         // Debug: Confirm the table creation
         tableEnv.executeSql("SHOW TABLES").print();
